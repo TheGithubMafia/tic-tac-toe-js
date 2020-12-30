505acbb9450288b2f92ff04bc86ddc28e1c4c1d8
TheGithubMafia <portfoliomafia@tuta.io>
2020-12-30 13:59:29 +0200

    getting there..

--- backend/server.js
+++ backend/server.js
@@ -14,15 +14,23 @@ io.on("connection", (client) => {
 	client.on("clicked", (id) => {
 		id = JSON.parse(id);
 		const [x, y] = id;
-		if (client.id === state[client.code].currentPlayer) {
-		state[client.code][x][y] = client.player;
-		// console.log("rooms: ", rooms);
+		if (client.player === state[client.code].currentPlayer) {
+			console.log("inside first if");
+			state[client.code][x][y] = client.player;
+			// console.log("rooms: ", rooms);
 			io.to(client.code).emit("update", `[${x}, ${y}]`, client.player);
-			if (rooms[client.code][0] === state[client.code].currentPlayer) {
-				state[client.code].currentPlayer = rooms[client.code][1]
+			if (
+				rooms[client.code][0].player ===
+				state[client.code].currentPlayer
+			) {
+				state[client.code].currentPlayer = rooms[client.code][1].player;
 			} else {
-				state[client.code].currentPlayer = rooms[client.code][0]
+				state[client.code].currentPlayer = rooms[client.code][0].player;
 			}
+			io.to(client.code).emit(
+				"currentPlayer",
+				state[client.code].currentPlayer
+			);
 		}
 
 		// console.log(state[`${client.code}`][x][y]);
@@ -35,37 +43,37 @@ io.on("connection", (client) => {
 		const code = String(Math.round(Math.random() * 1000000));
 		client.name = playerOne;
 		client.join(code);
-		rooms[code] = [client.id];
-		console.log(rooms);
 		client.player = "X";
+		console.log(rooms);
 		state[code] = [
 			[null, null, null],
 			[null, null, null],
 			[null, null, null],
 		];
-		
+
 		// state[code].currentPlayer = client.id;
-		console.log("current clientID", client.id)
+		console.log("current clientID", client.id);
 		// ensure user hasn't joined other rooms
 		client.code = code;
 		client.emit("gameCode", code);
 		state[code].playerOneName = playerOne;
-		client.emit('playerName', playerOne);
-		console.log(state[code])
+		client.emit("playerName", playerOne);
+		console.log(state[code]);
+		rooms[code] = [client];
 	});
 	client.on("joinGame", (code, playerTwo) => {
 		if (rooms[code].length === 2) return;
 		// ensure room doesn't have more than 2 players
 		client.join(code);
-		rooms[code].push(client.id);
 		console.log(rooms);
 		client.code = code;
 		client.name = playerTwo;
 		state[code].playerTwoName = playerTwo;
 		io.to(code).emit("playerNames", state[code].playerOneName, playerTwo);
 		client.player = "O";
-		state[code].currentPlayer = rooms[client.code][0];
-
+		state[code].currentPlayer = rooms[client.code][0].player;
+		io.to(code).emit("currentPlayer", state[code].currentPlayer);
+		rooms[code].push(client);
 		client.emit("gameCode", code);
 	});
 });
@@ -75,7 +83,7 @@ server.listen(3000);
 /* 
 close connections and delete resources when connection is closed
 
-add player names
+determine winner after each move
 
 refactor
 */
