b254d0b9a88aa8b08f83d18d5bbd828f731786ec
Asem <asem.mahmod@gmail.com>
2021-01-02 11:29:50 +0200

    a draw condition is checked after each move

--- server.js
+++ server.js
@@ -4,47 +4,12 @@ const path = require("path");
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const PORT = process.env.PORT || 3000;
-const { generateId } = require("./utils.js");
+const { generateId, findWinner } = require("./utils.js");
 
 const games = {};
 const boardState = {};
 
 app.use(express.static(path.join(__dirname, "./client")));
-
-function findWinner(grid) {
-	const [rowOne, rowTwo, rowThree] = grid;
-	const colOne = [rowOne[0], rowTwo[0], rowThree[0]];
-	const colTwo = [rowOne[1], rowTwo[1], rowThree[1]];
-	const colThree = [rowOne[2], rowTwo[2], rowThree[2]];
-	const diagonalOne = [rowOne[0], rowTwo[1], rowThree[2]];
-	const diagonalTwo = [rowOne[2], rowTwo[1], rowThree[0]];
-
-	if (
-		rowOne.every((item) => item === "X") ||
-		rowTwo.every((item) => item === "X") ||
-		rowThree.every((item) => item === "X") ||
-		colOne.every((item) => item === "X") ||
-		colTwo.every((item) => item === "X") ||
-		colThree.every((item) => item === "X") ||
-		diagonalOne.every((item) => item === "X") ||
-		diagonalTwo.every((item) => item === "X")
-	) {
-		return "X";
-	} else if (
-		rowOne.every((item) => item === "O") ||
-		rowTwo.every((item) => item === "O") ||
-		rowThree.every((item) => item === "O") ||
-		colOne.every((item) => item === "O") ||
-		colTwo.every((item) => item === "O") ||
-		colThree.every((item) => item === "O") ||
-		diagonalOne.every((item) => item === "O") ||
-		diagonalTwo.every((item) => item === "O")
-	) {
-		return "O";
-	} else {
-		return false;
-	}
-}
 
 io.on("connection", (client) => {
 	client.on("newgame", (playerName) => {
@@ -111,7 +76,7 @@ io.on("connection", (client) => {
 			// check if there's a winner
 			if (findWinner(boardState[client.gameId])) {
 				io.to(client.gameId).emit(
-					"winner",
+					"gameover",
 					findWinner(boardState[client.gameId])
 				);
 				boardState[client.gameId].hasBeenWon = true;
--- utils.js
+++ utils.js
@@ -1,3 +1,40 @@
 const generateId = () => Math.random().toString(16).slice(2);
 
-module.exports = { generateId };
+function findWinner(grid) {
+	const [rowOne, rowTwo, rowThree] = grid;
+	const colOne = [rowOne[0], rowTwo[0], rowThree[0]];
+	const colTwo = [rowOne[1], rowTwo[1], rowThree[1]];
+	const colThree = [rowOne[2], rowTwo[2], rowThree[2]];
+	const diagonalOne = [rowOne[0], rowTwo[1], rowThree[2]];
+	const diagonalTwo = [rowOne[2], rowTwo[1], rowThree[0]];
+
+	if (
+		rowOne.every((item) => item === "X") ||
+		rowTwo.every((item) => item === "X") ||
+		rowThree.every((item) => item === "X") ||
+		colOne.every((item) => item === "X") ||
+		colTwo.every((item) => item === "X") ||
+		colThree.every((item) => item === "X") ||
+		diagonalOne.every((item) => item === "X") ||
+		diagonalTwo.every((item) => item === "X")
+	) {
+		return "X";
+	} else if (
+		rowOne.every((item) => item === "O") ||
+		rowTwo.every((item) => item === "O") ||
+		rowThree.every((item) => item === "O") ||
+		colOne.every((item) => item === "O") ||
+		colTwo.every((item) => item === "O") ||
+		colThree.every((item) => item === "O") ||
+		diagonalOne.every((item) => item === "O") ||
+		diagonalTwo.every((item) => item === "O")
+	) {
+		return "O";
+	} else if (grid.every((row) => row.every((item) => item !== null))) {
+		return "It's a draw!";
+	} else {
+		return false;
+	}
+}
+
+module.exports = { generateId, findWinner };
